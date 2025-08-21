57eadcf99424a63ffb74b52994a660936f5aebb8
Joshua Jandrell <joshuarjandrell@gmail.com>
2025-08-21 10:24:54 +0200

    Josh Notebook WIP

--- utils/array2latex.py
+++ utils/array2latex.py
@@ -1,11 +1,11 @@
 from typing import Literal, List, Any, Iterable
 import numpy as np
 
-def array2latex(array:np.ndarray|Iterable|Iterable[Iterable], matrix_type:Literal['b', '']='b')->str:
+def array2latex(array:np.ndarray|Iterable|Iterable[Iterable], matrix_type:Literal['b', '']='b', fmt_str:str = "{}")->str:
     matrix_str = f"\\begin{{{matrix_type}matrix}}\n"
     for row in array:
         if isinstance(row, Iterable):
-            matrix_str += " & ".join([str(v) for v in row]) + '\\\\ \n'
+            matrix_str += " & ".join([fmt_str.format(v) for v in row]) + '\\\\ \n'
         else:
             matrix_str += f'{row} \\\\ \n'
     matrix_str += f"\\end{{{matrix_type}matrix}}"
